fc7ca156814c53a6efaefb3ad8d04a1a30fd16da
Pinch347 <128099413+Pinch347@users.noreply.github.com>
2023-03-18 15:41:13 -0500

    Update Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Email: contact@example.com</p>
+      <p>Phone: 555-555-5555</p>
+    </div>
+  );
+};
+
+export default Contact;
