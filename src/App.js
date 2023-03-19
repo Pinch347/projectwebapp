ce70ba543ae5f411d07e614c44d18fceb660f8da
Pinch347 <128099413+Pinch347@users.noreply.github.com>
2023-03-19 18:09:38 -0500

    Working version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import TodoList from './components/TodoList';
 import Contact from './components/Contact';
@@ -11,14 +11,10 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/todos">
-              <TodoList />
-            </Route>
-            <Route exact path="/contact">
-              <Contact />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/todos" element={<TodoList />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </div>
       </div>
     </Router>
